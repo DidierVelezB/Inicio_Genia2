b2b05a7d47a39815bee89cabfe267d44d4d93e1e
DidierVelezB <otalvarodidier@gmail.com>
2025-06-02 18:47:20 -0500

    Cambios minimos en el css

--- promociones/index.php
+++ promociones/index.php
@@ -4,13 +4,19 @@
     <meta charset="UTF-8">
     <title>Promociones</title>
     <link rel="stylesheet" href="styles.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
 </head>
 <body>
     <div class="container">
+        
         <header>
-            <div class="logo">
-                <h1>GENIA - Promociones</h1>
-            </div>
+            <div class="home-button">
+        <a href="../ropa_venta/index.php" class="home-link">
+            <i class="fa-solid fa-house-chimney"></i>
+            <span class="home">HOME</span>
+        </a>
+    </div>
+                <h1>PROMOCIONES</h1>
         </header>
 
         <div class="formulario">
